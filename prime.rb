aedb7c9ef804808ae511168d8c6f760f6ef4caac
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 22:34:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,17 @@
+# def prime? (int)
+#   if int < 2
+#     return false
+#   else
+#     divisors = (2...int).to_a
+#       divisors.each do |i|
+#       if int % i == 0
+#         return false
+#       end
+#     end
+#   end
+#   return true
+# end
+
 def prime? (int)
   if int < 2
     return false
@@ -8,6 +22,6 @@ def prime? (int)
         return false
       end
     end
+    return true
   end
-  return true
 end
